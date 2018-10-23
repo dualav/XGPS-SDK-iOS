319cb591162af0a03b256defd32e62f8c454ad3a
Huan Vu <huan.vu@evalabs.io>
2018-10-23 16:43:34 -0700

    Isolate dependencies so that only XGPSSDK folder can be copied to project

---
+++ XGPSSample/XGPSSDK/LogData.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+struct LogData {
+    let sig: Int
+    let interval: Int
+    let startBlock: Int
+    let countEntry: Int
+    let countBlock: Int
+    let createDate: String
+    let createTime: String
+    let fileSize: Int
+    let defaultString: String
+    var localFilename: String
+}
+
